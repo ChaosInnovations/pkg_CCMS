902bbed4340b85d6d14858c2af375e47fd694bed
chaosinnovator <36967580+chaosinnovator@users.noreply.github.com>
2022-09-02 19:16:52 -0700

    SetupController to enter setup view

---
+++ pkg/Database/Controllers/SetupController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Package\Database\Controllers;
+
+use LDAP\Result;
+use Package\CCMS\Controllers\BaseController;
+use Package\CCMS\Extensions\Route;
+use Package\CCMS\Extensions\RoutePrefix;
+use Package\CCMS\Models\HTTP\Method;
+use Package\CCMS\Models\Response;
+use Package\Database\Services\DatabaseService;
+
+class SetupController extends BaseController
+{
+    // These endpoints should only be enabled if the database configuration has not yet been set up.
+
+    #[Route(Method::GET, 'setup')]
+    public function SetupStart() : Response {
+        // check if we should do startup flow, otherwise return empty Response
+        if (!DatabaseService::Instance()->IsConnectionOpen()) {
+            return Response::GetEmpty();
+        }
+
+        // Finally, return Database Setup view
+        
+    }
+}
