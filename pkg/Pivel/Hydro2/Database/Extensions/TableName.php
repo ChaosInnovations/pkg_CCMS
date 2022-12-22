b5358773ebd3a793452a961b0ea610b65c0ab8d4
chaosinnovator <36967580+chaosinnovator@users.noreply.github.com>
2022-12-21 20:30:13 -0800

    new class property syntax

--- pkg/Pivel/Hydro2/Database/Extensions/TableName.php
+++ pkg/Pivel/Hydro2/Database/Extensions/TableName.php
@@ -6,10 +6,8 @@ use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 class TableName {
-    public string $tableName;
-
-    public function __construct(string $tableName)
-    {
-        $this->tableName = $tableName;
+    public function __construct(
+        public string $tableName,
+        ) {
     }
 }
