e5137979441f7bb30460c752fab8c15a5471dc5f
chaosinnovator <36967580+chaosinnovator@users.noreply.github.com>
2022-12-29 21:01:12 -0800

    Add html and alt bodies to emailmessage

--- pkg/Pivel/Hydro2/Email/Models/EmailMessage.php
+++ pkg/Pivel/Hydro2/Email/Models/EmailMessage.php
@@ -2,10 +2,23 @@
 
 namespace Package\Pivel\Hydro2\Email\Models;
 
+use Package\Pivel\Hydro2\Core\Views\BaseView;
+
 class EmailMessage
 {
+    private ?string $htmlBody;
+    private ?string $altBody;
+
     public function __construct()
     {
         
+    }
+
+    public function SetHTMLBody(BaseView $view) : void {
+        $this->htmlBody = $view->Render();
+    }
+
+    public function SetAltBody(BaseView $view) : void {
+        $this->altBody = $view->Render();
     }
 }
