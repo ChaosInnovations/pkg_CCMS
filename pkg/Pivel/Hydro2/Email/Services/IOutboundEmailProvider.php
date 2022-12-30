721e5ed2803c778ed0a45db33f30488ade67d773
chaosinnovator <36967580+chaosinnovator@users.noreply.github.com>
2022-12-29 21:00:50 -0800

    Add SendEmail method to emailprovider

--- pkg/Pivel/Hydro2/Email/Services/IOutboundEmailProvider.php
+++ pkg/Pivel/Hydro2/Email/Services/IOutboundEmailProvider.php
@@ -2,9 +2,12 @@
 
 namespace Package\Pivel\Hydro2\Email\Services;
 
+use Package\Pivel\Hydro2\Email\Models\EmailMessage;
 use Package\Pivel\Hydro2\Email\Models\OutboundEmailProfile;
 
 interface IOutboundEmailProvider
 {
     public function __construct(OutboundEmailProfile $profile);
+    
+    public function SendEmail(EmailMessage $message) : bool;
 }
--- pkg/Pivel/Hydro2/Email/Services/SMTPProvider.php
+++ pkg/Pivel/Hydro2/Email/Services/SMTPProvider.php
@@ -2,12 +2,16 @@
 
 namespace Package\Pivel\Hydro2\Email\Services;
 
+use Package\Pivel\Hydro2\Email\Models\EmailMessage;
 use Package\Pivel\Hydro2\Email\Models\OutboundEmailProfile;
 
 class SMTPProvider implements IOutboundEmailProvider
 {
-    public function __construct(OutboundEmailProfile $profile)
-    {
+    public function __construct(OutboundEmailProfile $profile) {
         
+    }
+
+    public function SendEmail(EmailMessage $message) : bool {
+        return true;
     }
 }
