b9b34a30b7dd307a6837bb0d4652694b71133ce6
chaosinnovator <36967580+chaosinnovator@users.noreply.github.com>
2023-01-14 23:43:17 -0800

    add methods to SMTPProvider for compiling RFC822 and multipart messages

--- pkg/Pivel/Hydro2/Email/Services/SMTPProvider.php
+++ pkg/Pivel/Hydro2/Email/Services/SMTPProvider.php
@@ -87,4 +87,76 @@ class SMTPProvider implements IOutboundEmailProvider
 
     protected static function CompileMessage(EmailMessage $message): string {
     }
+
+    /**
+     * @param string[] $headers fieldname=>value pairs.
+     *   Will ensure each header ends with exactly one LINE_ENDING.
+     *   Will make headers multi-line if needed.
+     * @param string $body
+     */
+    protected static function CompileRFC822Message(array $headers, string $body) : string {
+        $compiledHeaders = self::CompileHeaders($headers);
+        // remove leading/trailing whitespace from body
+        $body = trim($body);
+        // add an extra LINE_ENDING between headers and body
+        return $compiledHeaders . self::LINE_ENDING . $body . self::LINE_ENDING;
+    }
+
+    protected const MULTIPART_MIXED = 'multipart/mixed';
+    protected const MULTIPART_ALTERNATIVE = 'multipart/alternative';
+
+    /**
+     * @param string[] $headers fieldname=>value pairs.
+     *   Will ensure each header ends with exactly one LINE_ENDING.
+     *   Will make headers multi-line if needed.
+     * @param string[] $bodyParts array of already-compiled message parts.
+     *   Will add/ensure there are exactly two LINE_ENDINGs between bodyPart and the boundary
+     */
+    protected static function CompileMultipartMessage(array $headers, array $bodyParts, string $type=self::MULTIPART_MIXED, ?string $boundary=null) : string {
+        if (empty($boundary)) {
+            // default boundary starts with "=_" since this pattern is impossible in
+            //  both quoted-printable and base64 content.
+            $boundary = '=_'.bin2hex(random_bytes(8));
+        }
+        $boundarySeparator = self::LINE_ENDING . '--' . $boundary;
+
+        // add content-type header woth boundary
+        $headers['Content-Type'] = "{$type}; boundary=\"{$boundary}\"";
+
+        $compiledMessage = '';
+        
+        $compiledMessage .= self::CompileHeaders($headers);
+
+        $compiledMessage .= $boundarySeparator;
+
+        foreach ($bodyParts as $bodyPart) {
+            $compiledMessage .= self::LINE_ENDING;
+            $compiledMessage .= $bodyPart;
+            $compiledMessage .= $boundarySeparator;
+        }
+
+        // last boundary separator must end with an extra -- at the end.
+        $compiledMessage .= '--';
+        $compiledMessage .= self::LINE_ENDING;
+
+        return $compiledMessage;
+    }
+
+    /**
+     * @param string[] $headers fieldname=>value pairs.
+     *   Will ensure each header ends with exactly one LINE_ENDING.
+     *   Will make headers multi-line if needed.
+     */
+    protected static function CompileHeaders(array $headers) {
+        $compiledHeaders = '';
+        foreach ($headers as $fieldName => $fieldValue) {
+            $header = $fieldName.': '.$fieldValue;
+            // Strip existing \r and \n from each header
+            $header = str_replace(['\r', '\n'], '', $header);
+            // TODO: limit length/split into multi-line header if necessary.
+            // add to complied headers
+            $compiledHeaders .= $header . self::LINE_ENDING;
+        }
+        return $compiledHeaders;
+    }
 }
