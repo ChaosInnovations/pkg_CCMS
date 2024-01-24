3998bd1d1bfae650949c8a8bcb6314a8a0708427
chaosinnovator <thomas.boland@outlook.com>
2024-01-23 18:20:04 -0800

    Add remaining http status codes to js library

--- web/assets/Pivel/Hydro2/hydro2-2.3.0.1.js
+++ web/assets/Pivel/Hydro2/hydro2-2.3.0.1.js
@@ -96,16 +96,6 @@ var H = {
         }
     },
 
-    // TODO add remaining status codes
-    StatusCode: {
-        OK: 200,
-        Created: 201,
-        NoContent: 204,
-        BadRequest: 400,
-        NotFound: 404,
-        InternalServerError: 500,
-    },
-
     HtmlEncode: function(s) {
         var el = document.createElement('div');
         el.innerText = el.textContent = s;
@@ -292,5 +282,75 @@ var H = {
         Count() {
             return this._nodeList.length;
         }
-    }
+    },
+
+    StatusCode: {
+        Continue: 100,
+        SwitchingProtocols: 101,
+        Processing: 102,
+        EarlyHints: 103,
+        
+        OK: 200,
+        Created: 201,
+        Accepted: 202,
+        NonAuthoritativeInformation: 203,
+        NoContent: 204,
+        ResetContent: 205,
+        PartialContent: 206,
+        MultiStatus: 207,
+        AlreadyReported: 208,
+        IMUsed: 226,
+
+        MultipleChoices: 300,
+        MovedPermanently: 301,
+        Found: 302,
+        SeeOther: 303,
+        NotModified: 304,
+        UseProxy: 305,
+        //SwitchProxy: 306,
+        TemporaryRedirect: 307,
+        PermanentRedirect: 308,
+
+        BadRequest: 400,
+        Unauthorized: 401,
+        PaymentRequired: 402,
+        Forbidden: 403,
+        NotFound: 404,
+        MethodNotAllowed: 405,
+        NotAcceptable: 406,
+        ProxyAuthenticationRequired: 407,
+        RequestTimeout: 408,
+        Conflict: 409,
+        Gone: 410,
+        LengthRequired: 411,
+        PreconditionFailed: 412,
+        PayloadTooLarge: 413,
+        URITooLong: 414,
+        UnsupportedMediaType: 415,
+        RangeNotSatisfiable: 416,
+        ExpectationFailed: 417,
+        ImATeapot: 418,
+        MisdirectedRequest: 421,
+        UnprocessableEntity: 422,
+        Locked: 423,
+        FailedDependency: 424,
+        TooEarly: 425,
+        UpgradeRequired: 426,
+        PreconditionRequired: 428,
+        TooManyRequests: 429,
+        RequestHeaderFieldsTooLarge: 431,
+        UnavailableForLegalReasons: 451,
+
+        InternalServerError: 500,
+        NotImplemented: 501,
+        BadGateway: 502,
+        ServiceUnavailable: 503,
+        GatewayTimeout: 504,
+        HTTPVersionNotSupported: 505,
+        VariantAlsoNegotiates: 506,
+        InsufficientStorage: 507,
+        LoopDetected: 508,
+        NotExtended: 510,
+        NetworkAuthenticationRequired: 511,
+    },
 }
